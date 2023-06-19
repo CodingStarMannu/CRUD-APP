e299536da68e847e88d17b57004679ebc3a94428
Manoj_Pant <manojpant097@gmail.com>
2023-06-19 15:15:14 +0530

    first commit

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function index()
+    // {
+    //     return response()->json(User::get());
+    //     // return response()->json(User::latest()->get());
+    // }
+
+    public function index()
+{
+    $users = User::all();
+    return response()->json($users);
+}
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        User::create([
+            'id'=>$request->empId,
+            'EmpName'=>$request->empName,
+            'Email'=>$request->email,
+            'Contact'=>$request->contact,
+            'Age'=>$request->age
+        ]);
+        return response()->json('successfully created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return response()->json(User::whereId($id)->first());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::whereId($id)->first();
+        $user->update([
+            'id'=>$request->empId,
+            'EmpName'=>$request->empName,
+            'Email'=>$request->email,
+            'Contact'=>$request->contact,
+            'Age'=>$request->age
+        ]);
+        return response()->json('success');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::whereId($id)->first()->delete();
+        return response()->json('success');
+    }
+}
